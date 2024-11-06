c8351f067295ab0cb00fa3a5ea7a7914fbb66cc3
Madison Seroka <81598698+meseroka@users.noreply.github.com>
2024-11-06 12:29:47 -0700

    Initial Commit

---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by Madison  Seroka on 11/6/24.
+//
+
+import Testing
+@testable import Landmarks
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
